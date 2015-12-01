05f3e807e484ba9f3eaa8c486496b9e23e98ae6d
Brian <brikeats@gmail.com>
2015-11-30 20:09:03 -0600

    Working example with both edge distance transform and skeleton branch distance distance transform

---
+++ snakes.py
@@ -0,0 +1,125 @@
+import time
+from functools import partial
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import ndimage, optimize
+from skimage import filters, feature, morphology
+from skimage.morphology import disk
+
+
+
+def snake_energy(flattened_pts, edge_dist, corner_dist, alpha, beta, gamma):
+    """
+    Compute the energy associated with a proposed contour. The contour is defined
+    by N 2-dimensional points. The energy is comprised of external energy, which is
+    derived from the supplied distance images; and internal energy, which is computed
+    based only on the characteristics of the contour. Note that the image 
+    interpolation uses only 1st-order splines, which increases speed at the 
+    expense of accuracy.
+    
+    The current implementation was created for a closed contour. An open contour 
+    formulation should replace the periodic 'np.roll' calls by non-periodic end-off 
+    shifts.
+
+    Args:
+        flattened_pts ((2*N,)-shaped numpy array): A flattened list of the contour 
+            points, ordered so that adjacent points are consecutive in the list. 
+            Can be created by calling arr_2d.ravel() on an ordered (N,2)-shaped array 
+            of points.
+        edge_dist (2D numpy array): Distance transform of binary edge detector.
+        corner_dist (2D numpy array): Distance transform of binary interest point detector.
+        alpha (float): The relative weight given to unevenly spaced points. A higher
+            value encourages evenly-spaced points. Should be > 0.
+        beta (float): The weight given to local curvature. A higher value encourages
+            flat contours.
+        gamma (float): The relative weights given to the distance from edges or corners.
+            A gamma of 0 means to consider only the edges; gamma=1 means we use only
+            the corner distance image.
+    
+    Returns:
+        float: Image energy. (lower is better)
+    """
+
+    pts = np.reshape(flattened_pts, (len(flattened_pts)/2, 2))
+    
+    # external energy (favors low values of distance image)
+    dist_vals = ndimage.interpolation.map_coordinates(edge_dist, [pts[:,0], pts[:,1]], order=1)
+    edge_energy = np.sum(dist_vals)
+    dist_vals = ndimage.interpolation.map_coordinates(corner_dist, [pts[:,0], pts[:,1]], order=1)
+    corner_energy = np.sum(dist_vals)
+    external_energy = (1-gamma)*edge_energy + gamma*corner_energy
+
+    # spacing energy (favors equi-distant points)
+    prev_pts = np.roll(pts, 1, axis=0)
+    next_pts = np.roll(pts, -1, axis=0)
+    displacements = pts - prev_pts
+    point_distances = np.sqrt(displacements[:,0]**2 + displacements[:,1]**2)
+    mean_dist = np.mean(point_distances)
+    spacing_energy = np.sum((point_distances - mean_dist)**2)
+
+    # curvature energy (favors smooth curves)
+    curvature_1d = prev_pts - 2*pts + next_pts
+    curvature = (curvature_1d[:,0]**2 + curvature_1d[:,1]**2)
+    curvature_energy = np.sum(curvature)
+    
+    return external_energy + alpha*spacing_energy + beta*curvature_energy
+
+    
+def fit_snake(pts, corner_dist, edge_dist, alpha=0.1, beta=0.05, gamma=0.5, point_plot=None):
+    """
+    Fit an active contour model (aka snakes) based on some initial points and a 
+    feature image. Given a list of points as a starting point, it evolves the points
+    until they sit at a minimum of the energy function 'snake_energy'. This function
+    is not especially good at avoiding local minima, and it does not adapt the number 
+    of points in the contour. Therefore, it is most useful for "polishing up" and 
+    already good initial guess.
+
+    Args:
+        pts ((N,2)-shaped numpy array): A list of the contour points, ordered so that
+            adjacent points are consecutive in the list (ie, in clockwise or counter-
+            clockwise order).
+        edge_dist (2D numpy array): Distance transform of binary edge detector.
+        corner_dist (2D numpy array): Distance transform of binary interest point detector.
+        alpha (float): The weight given to unevenly spaced points. A higher value encourages
+            evenly-spaced points. Should be > 0.
+        beta (float): The weight given to local curvature. A higher value encourages
+            flat contours.
+        gamma (float): The relative weights given to the distance from edges or corners.
+            A gamma of 0 means to consider only the edges; gamma=1 means we use only
+            the corner distance image.
+        point_plot (matplotlib.lines.Line2D, optional): A matplotlib line object for
+            the given points. The Line2D data will be updated on each iteration to 
+            provide an animation of the optimization.
+    
+    Returns:
+        (N,2)-shaped numpy array: The points after minimization.
+    """
+    
+    if point_plot:
+        def callback_function(new_pts):
+            callback_function.nits += 1
+            y = new_pts[0::2]
+            x = new_pts[1::2]
+            point_plot.set_data(x,y)
+            plt.title('%i iterations' % callback_function.nits)
+            point_plot.figure.canvas.draw()
+            time.sleep(0.1)
+        callback_function.nits = 0
+    else:
+        callback_function = None
+    
+    # optimize
+    cost_function = partial(snake_energy, alpha=alpha, beta=beta, gamma=gamma, 
+                            edge_dist=edge_dist, corner_dist=corner_dist)
+    options = {'disp':False}
+    options['maxiter'] = 100  # FIXME: check convergence
+    method = 'BFGS'  # 'BFGS', 'CG', or 'Powell'. 'Nelder-Mead' has very slow convergence
+    res = optimize.minimize(cost_function, pts.ravel(), method=method, options=options, callback=callback_function)
+    optimal_pts = np.reshape(res.x, (len(res.x)/2, 2))
+
+    return optimal_pts
+
+
+
+
+
---
+++ snakes_demo.py
@@ -0,0 +1,116 @@
+from snakes import fit_snake
+import numpy as np
+from skimage import measure
+from scipy.interpolate import splprep, splev
+from scipy.integrate import simps
+from scipy.ndimage.filters import uniform_filter
+from scipy import ndimage
+from skimage import filters, feature, morphology
+import matplotlib.pyplot as plt
+
+
+def enhance_ridges(frame, mask=None):
+    """Detect ridges (larger hessian eigenvalue)"""
+    blurred = filters.gaussian_filter(frame, 2)
+    Hxx, Hxy, Hyy = feature.hessian_matrix(blurred, sigma=4.5, mode='nearest')
+    ridges = feature.hessian_matrix_eigvals(Hxx, Hxy, Hyy)[0]
+
+    return np.abs(ridges)
+
+
+
+def mask_to_boundary_pts(mask, pt_spacing=10):
+    """
+    Convert a binary image containing a single object to a set
+    of 2D points that are equally spaced along the object's contour.
+    """
+
+    # interpolate boundary
+    boundary_pts = measure.find_contours(mask, 0)[0]
+    tck, u = splprep(boundary_pts.T, u=None, s=0.0, per=1)
+    u_new = np.linspace(u.min(), u.max(), 1000)
+    x_new, y_new = splev(u_new, tck, der=0)
+
+    # get equi-spaced points along spline-interpolated boundary
+    x_diff, y_diff = np.diff(x_new), np.diff(y_new)
+    S = simps(np.sqrt(x_diff**2 + y_diff**2))
+    N = int(round(S/pt_spacing))
+
+    u_equidist = np.linspace(0, 1, N+1)
+    x_equidist, y_equidist = splev(u_equidist, tck, der=0)
+    return np.array(zip(x_equidist, y_equidist))
+
+
+
+# load data: the raw image an a binary region-of-interest image
+im = np.load('cropped_frame.npy')
+mask = np.load('enlarged_mask.npy')
+# mask = np.load('shifted_mask.npy')
+# mask = np.load('shrunken_mask.npy')
+# mask = np.load('target_mask.npy')
+
+
+# get boundary points of mask
+boundary_pts = mask_to_boundary_pts(mask, pt_spacing=5)
+x, y = boundary_pts[:,1], boundary_pts[:,0]
+
+
+# distance from ridge midlines
+ridges = enhance_ridges(im)
+thresh = filters.threshold_otsu(ridges)
+prominent_ridges = ridges > 0.8*thresh
+skeleton = morphology.skeletonize(prominent_ridges)
+edge_dist = ndimage.distance_transform_edt(-skeleton)
+edge_dist = filters.gaussian_filter(edge_dist, sigma=2)
+
+
+# distance from skeleton branch points
+blurred_skeleton = uniform_filter(skeleton.astype(float), size=3)
+corner_im = blurred_skeleton > 4./9
+corner_dist = ndimage.distance_transform_edt(-corner_im)
+corners_labels = measure.label(corner_im)
+corners = np.array([region.centroid for region in measure.regionprops(corners_labels)])
+
+
+
+# show the intermediate images
+plt.gray()
+plt.ion()
+plt.subplot(221)
+plt.imshow(im)
+plt.title('original image')
+plt.axis('off')
+plt.subplot(222)
+plt.imshow(ridges)
+plt.title('ridge filter')
+plt.axis('off')
+plt.subplot(223)
+plt.imshow(skeleton)
+plt.plot(corners[:,1], corners[:,0], 'ro')
+plt.title('ridge skeleton w/ branch points')
+plt.axis('off')
+plt.subplot(223)
+plt.imshow(skeleton)
+plt.autoscale(False)
+plt.plot(corners[:,1], corners[:,0], 'ro')
+plt.title('ridge skeleton w/ branch points')
+plt.subplot(224)
+plt.imshow(edge_dist)
+plt.title('distance transform of skeleton')
+plt.axis('off')
+plt.ioff()
+plt.show()
+
+
+# show an animation of the fitting procedure
+plt.ion()
+plt.figure()
+plt.imshow(im, cmap='gray')
+plt.plot(x, y, 'bo')
+line_obj, = plt.plot(x, y, 'ro')
+plt.axis('off')
+
+snake_pts = fit_snake(boundary_pts, edge_dist, corner_dist, alpha=0.5, beta=0.2, gamma=0.8, point_plot=line_obj)
+
+plt.ioff()
+plt.show()
